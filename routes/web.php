0814d67c080a958e3e8699263d2c971cd2258c01
Mohamad Azka Julda S <azkajulda@gmail.com>
2020-10-15 18:41:23 +0700

    Fix Conflict

--- routes/web.php
+++ routes/web.php
@@ -42,6 +42,7 @@ Route::group(['prefix' => 'admin-panel', 'namespace' => 'Admin', 'middleware' =>
         Route::get('/data-master', 'JobdescController@index')->name('data-job_description');
         Route::get('/add', 'JobdescController@create')->name('add-job_description');
         Route::post('/create', 'JobdescController@store')->name('create-job_description');
+      });
 
      Route::group(['prefix'=>'plan'], function() {
         Route::get('/data-master', 'PlanController@index')->name('data-plan');
